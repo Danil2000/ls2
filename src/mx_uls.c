f89e6f8f6794274b9b808a39bb9e96550af2047b
danil <noisyboy2000@gmail.com>
2020-01-25 18:08:10 +0200

    ready print for all flags

--- src/mx_uls.c
+++ src/mx_uls.c
@@ -8,28 +8,39 @@ static void mx_choose_flag(char *arg, char **args, DIR *dir) {
 	if (!mx_strcmp(arg, "-d"))
 		mx_ls_d(args);
 	if (!mx_strcmp(arg, "-a"))
-		mx_lsa(dir);
+		mx_lsa(dir, args);
 	if (!mx_strcmp(arg, "-A"))
 		mx_ls_A(dir, args);
 }
 
 static void choose_wf_d(char **argv) {
 	DIR *dir;
-	//mx_printstr("\x1b[34mTest\x1b[0m");
-	if (argc == 1) 	{
-		mx_ls_wd();
-		return 0;
-	}
-	mx_check(argc, argv);
+	//mx_printstr("\x1b[34mTes
 	if (argv[1][0] != '-') 	{
 		dir = opendir(argv[1]);
 		mx_check_dir(dir, argv);
 		mx_ls_wf(dir, argv);
+		exit(1);
+	}
+	if(mx_strcmp(argv[2], "") == 0) {
+	 	mx_ls_d(argv);
+		exit(1);
+	}
+}
+
+int main(int argc, char **argv) {
+	DIR *dir;
+
+	if (argc == 1)
+	{
+		mx_ls_wd();
 		return 0;
 	}
+	mx_check(argc, argv);
 	choose_wf_d(argv);
 	dir = opendir(argv[2]);
 	mx_check_dir(dir, argv);
 	mx_choose_flag(argv[1], argv, dir);
 	closedir(dir);
+	// system("leaks -q uls");
 }
