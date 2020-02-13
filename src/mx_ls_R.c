0b3913913898a9bd05d303846f3ad9b219b99d8e
Valleka <34207078+Valleka@users.noreply.github.com>
2020-02-13 21:46:18 +0200

    very good

--- src/mx_ls_R.c
+++ src/mx_ls_R.c
@@ -1,8 +1,7 @@
 #include "uls.h"
 
 //она не срабатывает
-bool mx_is_dir(char *fname)
-{
+bool mx_is_dir(char *fname) {
 	struct stat f;
 	stat(fname, &f);
 	if (S_ISDIR(f.st_mode))
@@ -12,34 +11,32 @@ bool mx_is_dir(char *fname)
 	return false;
 }
 
-void mx_ls_R(char *fn)
-{
+void mx_ls_R(char *fn) {
 	DIR *dir;
 	struct dirent *files;
 	char *s;
 	char *s1;
+	DIR *dir1;
 
 	dir = opendir(fn);
+	dir1 = opendir(fn);
+	mx_ls_wf(dir1, fn);
 	files = readdir(dir);
-	//free(files);
 	files = readdir(dir);
 	while ((files = readdir(dir)) != NULL) {
 		if (mx_is_dir(files->d_name)) {
 			if (files->d_name[0] != '.') {
 				mx_printchar('\n');
-				mx_printstr(files->d_name);
-				mx_printchar(':');
-				mx_printchar('\n');
 				s = mx_strjoin(fn, "/");
 				s1 = mx_strjoin(s, files->d_name);
+				mx_printstr(s1);
+				mx_printchar(':');
+				mx_printchar('\n');
 				mx_ls_R(s1);
-			}
-		}
-		else {
-			if (files->d_name[0] != '.') {
-				mx_printstr(files->d_name);
-				mx_printspaces(24, mx_strlen(files->d_name));
+				free(s);
+				free(s1);
 			}
 		}
 	}
+	closedir(dir);
 }
